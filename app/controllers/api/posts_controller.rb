80c91733a863c6e7d7b4e4b0102e4d2113a52499
joshsarna <joshsarna@gmail.com>
2018-09-11 13:39:05 -0700

    make index and show actions for Post

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,11 @@
 class Api::PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render 'show.json.jbuilder'
+  end
 end
---
+++ app/views/api/posts/_post.json.jbuilder
@@ -0,0 +1,5 @@
+json.id post.id
+json.title post.title
+json.text post.text
+json.created_at post.created_at
+json.updated_at post.updated_at
