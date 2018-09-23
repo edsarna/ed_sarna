adf0718905dd04b3d6a17b3ec676ee902bbe1fb5
joshsarna <joshsarna@gmail.com>
2018-09-23 01:07:40 -0700

    add award pictures to img folder and add stories through seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,74 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Publication.create(
+#   title: "",
+#   media_type: "",
+#   short_blurb: "",
+#   long_blurb: "",
+#   pub_date: "",
+#   full_text: "",
+#   url: ""
+# )
+
+# Award.create(
+#   title: "",
+#   text: "",
+#   url: "",
+#   publication_id: Publication.find_by(name: "").id,
+#   date_received: ""
+# )
+
+# Publication.create(
+#   title: "The Defibrillator Meal",
+#   media_type: "short story",
+#   short_blurb: "Winner of the Bo Carter Memorial Contest for Nostalgia, and Jade Ring Contest for Humor.",
+#   long_blurb: "Winner of the Bo Carter Memorial Contest for Nostalgia, and Jade Ring Contest for Humor."
+# )
+
+# Award.create(
+#   title: "Bo Carter Memorial Contest",
+#   text: "First Place in Nostalgia",
+#   url: "https://waukeshawritersworkshop.org/2016-winners/",
+#   publication_id: Publication.find_by(title: "The Defibrillator Meal").id,
+#   date_received: DateTime.new(2017,1,1,0,0,0)
+# )
+
+# Award.create(
+#   title: "Jade Ring Contest",
+#   text: "Second Place in Humor",
+#   url: "https://wiwrite.org/contests/",
+#   publication_id: Publication.find_by(title: "The Defibrillator Meal").id,
+#   date_received: DateTime.new(2016,6,15,0,0,0)
+# )
+
+# Publication.create(
+#   title: "Best Day Ever",
+#   media_type: "short story",
+#   short_blurb: "Winner of the Jade Ring Contest for Fiction.",
+#   long_blurb: "Winner of the Jade Ring Contest for Fiction.",
+#   url: "https://www.amazon.com/Creative-Wisconsin-Literary-Journal-2017/dp/0982842856/"
+# )
+
+# Award.create(
+#   title: "Jade Ring Contest",
+#   text: "Third Place in Fiction",
+#   url: "https://wiwrite.org/contests/",
+#   publication_id: Publication.find_by(title: "Best Day Ever").id,
+#   date_received: DateTime.new(2016,6,15,0,0,0)
+# )
+
+# Publication.create(
+#   title: "Dwayne Danger, Private Eye",
+#   media_type: "short story",
+#   short_blurb: "Florence Lindemann Humor Contest winner.",
+#   long_blurb: "Florence Lindemann Humor Contest winner."
+# )
+
+# Award.create(
+#   title: "Florence Lindemann Humor Contest",
+#   text: "Third Place",
+#   publication_id: Publication.find_by(title: "Dwayne Danger, Private Eye").id,
+#   date_received: DateTime.new(2015,1,1,0,0,0)
+# )
